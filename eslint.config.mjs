c0ca0d1c09b22f690e3cba970ef7f422ef444c52
AminPerez <joseamin1998@gmail.com>
2026-05-08 13:16:39 +0200

    FEAT: initiated npm project: installed vite + linter

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"],
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+    languageOptions: { globals: globals.browser }
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+]);
